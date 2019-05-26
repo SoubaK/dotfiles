a333def8256dc8b5ccaa3315c32a6cae5de226bd
SoubaK <souba.k@openmailbox.org>
2019-05-26 02:30:31 +0200

    Emacs init file v2

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
@@ -21,4 +21,41 @@
 
 (use-package highlight-indentation
   :ensure t
-  :config (add-hook 'prog-mode-hook #'highlight-identation-mode))
+  :config (add-hook 'prog-mode-hook 'highlight-indentation-mode))
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+;(use-package python-mode
+;  :ensure t)
+
+(use-package magit
+  :ensure t)
+
+(use-package fold-this
+  :ensure t
+  :bind (("C-x C-<up>" . fold-active-region)
+	 ("C-x C-<down>" . fold-this-unfold-at-point)))
+
+;;; Themes configuration
+(use-package helm-themes
+  :ensure t)
+
+(use-package monokai-theme
+  :ensure t )
+
+(use-package spacemacs-theme
+  :ensure t)
+
+(use-package sublime-themes
+  :ensure t)
+
+(use-package ample-theme
+  :ensure t)
+
+(use-package darkokai-theme
+  :ensure t)
+
+(use-package solarized-theme
+  :ensure t)
